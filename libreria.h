ef90730179bd6222562f0c1e4c56b9c212952051
rodrigo <68453000+andresico01@users.noreply.github.com>
2020-09-28 12:49:19 -0500

    Update libreria.h

--- libreria.h
+++ libreria.h
@@ -1,67 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-//#define N_est 10
-typedef char string[20];
+//#include"texto_lista.h"
+typedef char texto[20];
 typedef struct{
-    string nombre;
-    string apellido;
+    texto nombre;
+    texto apellido;
     int edad;
     int peso;
     int numero_estudiantes;
-    string estudiantes[10];
+    texto estudiantes[10];
 }personas;
-
 typedef struct{
-    personas un_vale;
-    struct node* siguiente; 
-}node;
-
-node* lista = NULL;
-node* new(){                                //si teacher esto es de su codigo.
-    node* nuevo = malloc(sizeof(node));
-    nuevo->siguiente =  NULL;
-    return (nuevo);
+    personas datos;
+    struct Nodo* ptrNext;
+} Nodo;
+Nodo *header = NULL;
+Nodo *footer = NULL;
+void limpiar(void){
+    while (getchar()!='\n');
 }
-
-personas ingreso_datos(){
-    personas referencia;
-    int N_estu = 0, i = 0;
-    // fflush(stdin);
-    printf("ingrese los datos de referencia del asesino\n");
-    printf("\n ingrese el nombre del docente\n");
-    scanf("%s",referencia.nombre);
-    printf("\n ingrese el apellido del docente\n");
-    scanf("%s",referencia.apellido);
-    printf("\n ingrese su edad\n");
-    scanf("%d",&referencia.edad);
-    printf("\n ingrese peso \n");
-    scanf("%d",&referencia.peso);
-    fflush(stdin);
-    printf("\n ingrese numero de estudiantes\n");
-    scanf("%d",&referencia.numero_estudiantes);
-    do{
-    fflush(stdin);
-        printf("\ningrese el nombre del estudiante\n");
-        scanf("%s",referencia.estudiantes[i]);
-        i++;
-   }while(i < referencia.numero_estudiantes);
-    return (referencia);
-}
-void insertar_lista(){
-        node* new_nodo = new();
-        new_nodo->un_vale = ingreso_datos();
-        node* aux = lista;
-        node* aux_2;
-        while((aux != NULL) /*&& (aux->un_vale.peso < new_nodo->un_vale.peso)*/){
-            aux_2 = aux;
-            aux = aux->siguiente;
-        }
-        if(lista == aux){
-            lista = new_nodo;
-        }else aux_2->siguiente = new_nodo;
-        new_nodo->siguiente = aux;
-}
+int potencia(int base, int exp){
+    int res;
+    if (exp == 0){
+        res=1.0;
+    }else if (exp > 0){
+        res=base * potencia(base,exp - 1);
+    }
+    else if (exp < 0){
+        res= 1.0 / base *potencia(base,exp + 1);
+    }
+    return res;
+} 
 void mostrar_datos(personas datos){
     int i=0;
     printf("\nnombre del docente: %s\n", datos.nombre);
@@ -75,53 +44,183 @@ void mostrar_datos(personas datos){
     }
     printf("\n");
 }
-void mostrar_lista(){
-    node* actual = new();
-    actual = lista;
+Nodo *new(personas gente){
+    Nodo* nuevo = (Nodo*) malloc(sizeof(Nodo));
+    nuevo->datos = gente;
+    nuevo->ptrNext = NULL;
+    return(nuevo);
+}
+personas ingreso_datos(){
+    int i = 0;
+    personas referencia;
+
+    fflush(stdin);
+    printf("ingrese los datos de referencia del  docente\n");
+    printf("\n ingrese el nombre del docente\n");
+    scanf("%s",referencia.nombre);
+   
+    fflush(stdin);
+    printf("\n ingrese el apellido del docente\n");
+    scanf("%s",referencia.apellido);
+  
+    printf("\n ingrese su edad\n");
+    scanf("%d",&referencia.edad);
+    fflush(stdin);
+    printf("\n ingrese peso \n");
+    scanf("%d",&referencia.peso);
+    printf("\n ingrese numero de estudiantes\n");
+    scanf("%d",&referencia.numero_estudiantes);
     
-    while(actual != NULL){
-        
-        mostrar_datos(actual->un_vale);
-        actual = actual->siguiente;
+    do{
+        printf("\ningrese el nombre del estudiante\n");
+        scanf("%s",referencia.estudiantes[i]);
+        i++;
+   }while(i < referencia.numero_estudiantes);
+  // limpiar();
+   return referencia;
+}
+void insertar(Nodo *nodo){
+    if(header == NULL){
+        header = nodo;
+            
+        footer = nodo;
     }
+    else{
+        footer->ptrNext = nodo;
+        footer = nodo;
+    }
+}
+void run(){
+    Nodo *i = header;
+    if(i==NULL){
+        printf("lista vacía\r\n");
+        return;
+    }
+    else{
+        while(i != NULL){
+            mostrar_datos(i->datos);
+            i = i->ptrNext;
+        }
+        printf("\r\n");
+    }
+}
+void free_memory(){
+    Nodo *i = header;
+    if(i==NULL){
+        printf("lista vacia\r\n");
+        return;
+    }
+    else{
+        while(i != NULL){
+            header = i->ptrNext;
+            free(i);
+            i = header;
+        }
+        header = NULL;
+        footer = NULL;
+        printf("Memoria Liberada\r\n");
+    }
+}
+void pop(unsigned char index){
+    unsigned char cont = 0;
+    Nodo *i = header;
+    Nodo *ant = NULL;
+    //index--;
+    if( i == NULL){
+        printf("lista vacía\r\n");
+        return;
+    }
+    else{
+       while( i != NULL && cont != index){
+           ant = i;
+           i = i->ptrNext;
+           cont++;
+       }
+       if (i == NULL){
+           printf("\nla lista no es tan extensa, vaya a la opcion 1 a insertar datos\n");
+       }
+       else if (ant == NULL){
+           header = header->ptrNext;
+           free (i);
+       }
+       else{
+           ant->ptrNext = i->ptrNext;
+           free(i);
+       }
+    }
+}
+void insertar_grupo(personas grupo, int posicion){
+    Nodo *new_node = new(grupo);                    
+    unsigned char cont = 0;                                           
+
+    Nodo *aux = header;                               
+    Nodo *aux2 = NULL;                                     
+    while((aux != NULL) && ( cont < posicion)){         
+        aux2 = aux;                                 
+        aux = aux->ptrNext;     
+        cont++;                  
+    }                                               
+    
+    if(header == aux){
+        header = new_node;
+    }else if(aux == NULL){
+        printf("\nla lista es muy pequena para la posicion solicitada\n");
+        return;
+    }else aux2->ptrNext = new_node;
+    new_node->ptrNext = aux;
+
 }
 void menu(){
     //personas docentes = ingreso_datos();
-    int opcion = 0;
+    personas gente;
+    Nodo* nuevo_nodo = NULL;
+    
+    unsigned int opcion = 0, cont = 0, posicion;
+    //personas gente;
     do{
-        fflush(stdin);
-        printf("\nque opcion desea ?\n");
+        printf("\n1)ingresar datos a la lista \n2)mostrar los grupos de la lista  \n3)agregar algun grupo a la lista \n4)eliminar un grupo en especial\n5)salir");
+        printf("\nque opcion desea?\n");
         scanf("%d",&opcion);
+        fflush(stdin);
         switch (opcion){
             
             case 1 : 
-                insertar_lista();
+                gente = ingreso_datos();
+                nuevo_nodo = new(gente);
+                insertar(nuevo_nodo);
+                free(nuevo_nodo);
+                nuevo_nodo = nuevo_nodo->ptrNext;
+                cont++;
+                
                 system("pause");
                 break;
             case 2 :
-                mostrar_lista();
+                run();
+               // fflush(stdin);
                 system("pause");
                 break;
+            case 3 :
+                //fflush(stdin);
+                
+                printf("\ningrese la posicion en la que desea agregar el nuevo valor.\nsi esta es mayor al tamaño de la lista habra error\n");
+                scanf("%d",&posicion);
+                gente = ingreso_datos();    
+                insertar_grupo(gente,posicion);
+                system("pause");
+                 break;
+            case 4 : 
+              //  fflush(stdin);
+                printf("\ningrese la posicion en la que desea elimnar.\nsi esta es mayor a el tamaño de la lista habra error\n");
+                scanf("%d",&posicion);
+                pop(posicion);
+                system("pause");
+                break;    
         }
   
     system("cls");
-    }while(opcion < 3);  
+    }while(opcion < 5);  
 }
-/*ingresa los datos guardados en gente y los pone en mi lista*/
-void insertar_texto_lista(personas nueva){
-        node* new_nodo = new();
-        new_nodo->un_vale = nueva;
-        node* aux = lista;
-        node* aux_2;
-        while((aux != NULL) /*&& (aux->un_vale.peso < new_nodo->un_vale.peso*/){
-            aux_2 = aux;
-            aux = aux->siguiente;
-        }
-        if(lista == aux){
-            lista = new_nodo;
-        }else aux_2->siguiente = new_nodo;
-        new_nodo->siguiente = aux;
-}
+
 /*paso los datos del texto a un vector, en la mayoria de casos el vector es el string que cree arriba*/
 void pasando_datos(char* datos, int posicion_0, int posicion_final,char string[]){
     int iterador_k = 0,iterador_o = 0;
@@ -134,14 +233,18 @@ void pasando_datos(char* datos, int posicion_0, int posicion_final,char string[]
 int cargando_datos(char valor[],int iterador_j){
     int iterador_k=0;
     int auxiliar = 0;
-    for(iterador_k = iterador_j-1;iterador_k >= 0;iterador_k--){
-        auxiliar += ((valor[(iterador_j-1)-iterador_k]-'0')*pow(10,iterador_k));
+   //printf("\n%d\n",iterador_j-2);
+    for(iterador_k = iterador_j-2;iterador_k >= 0;iterador_k--){
+        auxiliar += ((valor[(iterador_j-2)-iterador_k]-'0')*potencia(10,iterador_k));
+       // printf("%d\t",auxiliar);
     }
+   // printf("\n");
     return auxiliar;
 }
 /*los string al no inicializarse guardan valores basura en sus especios de memoria, aqui los "limpios" para que no me guareden resultados basura*/
 void debbugeando_madres(char vector[]){
-    for(int i = 0;i <= 20;i++)vector[i] = '\0';
+    int i = 0;
+    for( i = 0;i <= 20;i++)vector[i] = '\0';
 }
 /*este funcion la cree para reemplazar a free en los vectores "dinamicos" que creo en funciones que devuelven valores */
 void eliminar_vector(int* vec, int cont_3){
@@ -166,20 +269,17 @@ void inicio_posiciones(int vec[], unsigned char* datos){
             }
         i++;
     }
-    
-
 }
 /*esta funcion se encarga de encontrar las datos que corresponden a cada area de mi estructura*/
-personas pasando_lista(unsigned char* datos, int posicion){
+void pasando_lista(personas* gente,unsigned char* datos, int posicion){
     int vec[5] = {0};            
-    int* vec_2 = malloc(sizeof(int));          
+    int* vec_2 = (int*)malloc(sizeof(int));          
     int cont = 0;
     int cont_3 = 0;
     char valor[]="  ", valor_2[]="  ";
     int iterador_i = posicion, iterador_j = 0, iterador_k = 0, iterador_p=posicion,iterador_o=0;            
-    personas gente;
-    debbugeando_madres(gente.nombre);
-    debbugeando_madres(gente.apellido);
+    debbugeando_madres(gente->nombre);
+    debbugeando_madres(gente->apellido);
     while (*(datos+iterador_i) != '}' ){
         if (*(datos+iterador_i) == ':'){
             vec[cont] = iterador_i+1;
@@ -195,18 +295,22 @@ personas pasando_lista(unsigned char* datos, int posicion){
         }
             iterador_p++;
     } 
-    *(vec_2+6) = iterador_p-2;
+    *(vec_2+6) = iterador_p-3;
     iterador_k=0;
-    pasando_datos(datos,vec[0],*(vec_2+0),gente.nombre);
-    pasando_datos(datos,vec[1],*(vec_2+1),gente.apellido);
+    
+    pasando_datos(datos,vec[0]+1,*(vec_2+0),gente->nombre);
+    
+    pasando_datos(datos,vec[1]+1,*(vec_2+1),gente->apellido);
+    
     iterador_k = *(vec_2+2)+1;
-    pasando_datos(datos,vec[2]-1,iterador_k,valor);
+    pasando_datos(datos,vec[2],iterador_k,valor);
     iterador_k = (*(vec_2+2)+1)-vec[2];
-    gente.edad = cargando_datos(valor,iterador_k);
+    gente->edad = cargando_datos(valor,iterador_k);
+    iterador_k = 0;
     iterador_k = *(vec_2+3) + 1;
-    pasando_datos(datos,vec[3]-1,iterador_k,valor_2);
+    pasando_datos(datos,vec[3],iterador_k,valor_2);
     iterador_k = (*(vec_2+3)+1)-vec[3];
-    gente.peso = cargando_datos(valor_2,iterador_k);
+    gente->peso = cargando_datos(valor_2,iterador_k);
     iterador_k=0;
     iterador_j=0;
     iterador_i = vec[4];
@@ -214,17 +318,18 @@ personas pasando_lista(unsigned char* datos, int posicion){
         if (*(datos+iterador_i+iterador_k) == ',')iterador_j++;   
         iterador_k++;
     }
-   
-    for(int contando = 0;contando <=iterador_j ;contando++){
-        debbugeando_madres(gente.estudiantes[contando]);
+    int contando = 0;
+    for(contando = 0;contando <=iterador_j ;contando++){
+        debbugeando_madres(gente->estudiantes[contando]);
     }
-    gente.numero_estudiantes = iterador_j+1;
+    gente->numero_estudiantes = iterador_j+1;
     iterador_i=0;
     iterador_k=0;
-    pasando_datos(datos,vec[4]+1,*(vec_2+4),gente.estudiantes[0]);
+    pasando_datos(datos,vec[4]+2,*(vec_2+4),gente->estudiantes[0]);
+    
    for(iterador_i = 1;iterador_i <= iterador_j;iterador_i++){
-       pasando_datos(datos,*(vec_2+3+iterador_i)+2,*(vec_2+4+iterador_i),gente.estudiantes[iterador_i]);
+       pasando_datos(datos,*(vec_2+3+iterador_i)+3,*(vec_2+4+iterador_i),gente->estudiantes[iterador_i]);
+       
     }
    eliminar_vector(vec_2,cont_3);
-   return gente;
 }
